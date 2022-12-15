7946cccd7e709bb272abaaefa6245e771dc250b0
will.liu <kya9988@yahoo.com.tw>
2022-12-15 21:26:07 +0800

    init

---
+++ lotterypicker/src/test/java/idv/lottery/picker/lotterypicker/AppTest.java
@@ -0,0 +1,20 @@
+package idv.lottery.picker.lotterypicker;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
